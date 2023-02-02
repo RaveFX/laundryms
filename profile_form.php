1badb6363ba46c2e8eb9259738c516759085ddd2
ravefx <ravindhuprabasha@gmail.com>
2023-02-02 12:13:32 +0530

    updates

--- profile_form.php
+++ profile_form.php
@@ -1,38 +1,34 @@
 <?php
 session_start();
-// print_r($_SESSION["user_id"]);
 $emailFromDB =  $_SESSION["user_id"];
 
-      //$connection = mysqli_connect('localhost','root','','book_db');
-      $connection = new mysqli('localhost', 'root', '', 'laundry_db');
-      if(isset($_POST['send'])){
-         $name = $_POST['name'];
-         $phone = $_POST['phone'];
-         $address = $_POST['address'];
-         if($name==null || $phone==null || $address==null )
-         {
-            echo '<script>alert("Fields cannot be empty");location.href="http://localhost:998/laundryms/profile.php";</script>';
-         } else{
-            $request = "update signup_form set name ='$name',phone ='$phone',address ='$address' where email ='$emailFromDB'" ;
-            if ($connection->query($request)) {
-               $_POST = array();
-               echo '<script>alert("Profile Updated Successfully.");location.href="http://localhost:998/laundryms/home.php";</script>';
+$connection = new mysqli('localhost', 'root', '', 'laundry_db');
+if(isset($_POST['send'])){
+  $name = mysqli_real_escape_string($connection, $_POST['name']);
+  $phone = mysqli_real_escape_string($connection, $_POST['phone']);
+  $address = mysqli_real_escape_string($connection, $_POST['address']);
 
-            } else {
-            echo "Error: " . $request . "<br>" . $connection->error;
-            echo '<script>alert("Profile Update Failed.Please try Again");location.href="http://localhost:998/laundryms/profile.php";</script>';
-            }
-         }
-      }else{
-         echo 'something went wrong please try again!';
-      }
-
-   ?>
+  if($name==null || $phone==null || $address==null )
+  {
+     echo '<script>alert("Fields cannot be empty");location.href="http://localhost:998/laundryms/profile.php";</script>';
+  } else{
+     $request = "UPDATE signup_form SET name =?,phone =?,address =? WHERE email =?";
+     $stmt = $connection->prepare($request);
+     $stmt->bind_param("ssss", $name, $phone, $address, $emailFromDB);
+     if ($stmt->execute()) {
+        $_POST = array();
+        echo '<script>alert("Profile Updated Successfully.");location.href="http://localhost:998/laundryms/home.php";</script>';
+     } else {
+        echo "Error: " . $request . "<br>" . $connection->error;
+        echo '<script>alert("Profile Update Failed.Please try Again");location.href="http://localhost:998/laundryms/profile.php";</script>';
+     }
+  }
+}else{
+  echo 'something went wrong please try again!';
+}
+?>
 <html>
 <link rel="stylesheet" href="css/style.css">
 <center>
-<!-- <h1>Booking created successfully,sit back we'll contact you soon.</h1><br>
-<div class="btn" onclick="location.href='http://localhost:998/laundryms/home.php';">Return To Home</div> -->
-
 </center>
-   </html> 
+</html>
--- signup_form.php
+++ signup_form.php
@@ -1,36 +1,65 @@
 <?php
       
+    // $connection = new mysqli('localhost', 'root', '', 'laundry_db');
+    // if(isset($_POST['send'])){
+    // $name = $_POST['name'];
+    // $email = $_POST['email'];
+    // $password=$_POST['password'];
+    // $phone = $_POST['phone'];
+    // $address = $_POST['address'];
+    // $securityquestion = $_POST['securityquestion'];
+    // $securityanswer = $_POST['securityanswer'];
+
+    // if($name==null || $email==null || $password==null || $phone==null || $address==null || $securityquestion==null || $securityanswer==null)
+    // {
+    //    echo '<script>alert("Fields cannot be empty");location.href="http://localhost:998/laundryms/signup.php";</script>';
+    // } else{
+    // $request = " insert into signup_form(name, email, password, phone, address, securityquestion, securityanswer) values('$name','$email', '$password', '$phone','$address','$securityquestion','$securityanswer') ";
+    //      //mysqli_query($connection, $request);
+    //         if ($connection->query($request)) {
+    //         //echo'hii';
+    //         //echo'<script>alert("New record created successfully");</script>';
+    //         $_POST = array();
+    //         echo '<script>alert("Account Created Successfully.Please Login.");location.href="http://localhost:998/laundryms/index.php";</script>';
+
+    //         } else {
+    //             echo "Error: " . $request . "<br>" . $connection->error;
+    //         }
+    //      //header('location:book.php'); 
+    //     }
+    // }
+    //     else{
+    //     echo 'something went wrong please try again!';
+    // }
+
+    //corrected
     $connection = new mysqli('localhost', 'root', '', 'laundry_db');
-    if(isset($_POST['send'])){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password=$_POST['password'];
-    $phone = $_POST['phone'];
-    $address = $_POST['address'];
-    $securityquestion = $_POST['securityquestion'];
-    $securityanswer = $_POST['securityanswer'];
 
-    if($name==null || $email==null || $password==null || $phone==null || $address==null || $securityquestion==null || $securityanswer==null)
-    {
-       echo '<script>alert("Fields cannot be empty");location.href="http://localhost:998/laundryms/signup.php";</script>';
-    } else{
-    $request = " insert into signup_form(name, email, password, phone, address, securityquestion, securityanswer) values('$name','$email', '$password', '$phone','$address','$securityquestion','$securityanswer') ";
-         //mysqli_query($connection, $request);
-            if ($connection->query($request)) {
-            //echo'hii';
-            //echo'<script>alert("New record created successfully");</script>';
-            $_POST = array();
-            echo '<script>alert("Account Created Successfully.Please Login.");location.href="http://localhost:998/laundryms/index.php";</script>';
+if (isset($_POST['send'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $phone = $_POST['phone'];
+  $address = $_POST['address'];
+  $securityquestion = $_POST['securityquestion'];
+  $securityanswer = $_POST['securityanswer'];
 
-            } else {
-                echo "Error: " . $request . "<br>" . $connection->error;
-            }
-         //header('location:book.php'); 
-        }
+  if ($name == null || $email == null || $password == null || $phone == null || $address == null || $securityquestion == null || $securityanswer == null) {
+    echo '<script>alert("Fields cannot be empty");location.href="http://localhost:998/laundryms/signup.php";</script>';
+  } else {
+    $stmt = $connection->prepare("INSERT INTO signup_form (name, email, password, phone, address, securityquestion, securityanswer) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssss", $name, $email, $password, $phone, $address, $securityquestion, $securityanswer);
+
+    if ($stmt->execute()) {
+      $_POST = array();
+      echo '<script>alert("Account Created Successfully.Please Login.");location.href="http://localhost:998/laundryms/index.php";</script>';
+    } else {
+      echo "Error: " . $connection->error;
     }
-        else{
-        echo 'something went wrong please try again!';
-    }
+  }
+} else {
+  echo 'something went wrong please try again!';
+}
 ?>
 <link rel="stylesheet" href="css/style.css">
 <!-- <center>
