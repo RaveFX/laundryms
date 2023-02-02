7bc6916dd00ed16db5f0f34ee95287d8fac9c841
ravefx <ravindhuprabasha@gmail.com>
2023-02-02 11:53:25 +0530

    Update book_form.php

--- book_form.php
+++ book_form.php
@@ -2,32 +2,45 @@
       //$connection = mysqli_connect('localhost','root','','book_db');
       $connection = new mysqli('localhost', 'root', '', 'laundry_db');
       if(isset($_POST['send'])){
-         $date = $_POST['date'];
-         $topwear = $_POST['topwear'];
-         $bottomwear = $_POST['bottomwear'];
-         $woolen = $_POST['woolen'];
-         $others = $_POST['others'];
-         $servicetype = $_POST['servicetype'];
-         $name = $_POST['name'];
-         $email = $_POST['email'];
-         $phone = $_POST['phone'];
-         $address = $_POST['address'];
-         $description = $_POST['description'];
+         $date = htmlspecialchars($_POST['date']);
+   $topwear = htmlspecialchars($_POST['topwear']);
+   $bottomwear = htmlspecialchars($_POST['bottomwear']);
+   $woolen = htmlspecialchars($_POST['woolen']);
+   $others = htmlspecialchars($_POST['others']);
+   $servicetype = htmlspecialchars($_POST['servicetype']);
+   $name = htmlspecialchars($_POST['name']);
+   $email = htmlspecialchars($_POST['email']);
+   $phone = htmlspecialchars($_POST['phone']);
+   $address = htmlspecialchars($_POST['address']);
+   $description = htmlspecialchars($_POST['description']);
+
 
          if($date==null and $topwear==null and $bottomwear==null and $woolen==null and $others==null and $servicetype == null and $name==null and $email==null and $phone==null and $address==null and $description==null)
          {
             echo '<script>alert("Fields cannot be empty");location.href="http://localhost:998/laundryms/book.php";</script>';
          } else{
-            $request = "insert into book_form(date, topwear, bottomwear, woolen, others, servicetype, name, email, phone, address, description) values('$date','$topwear','$bottomwear','$woolen', '$others', '$servicetype', '$name', '$email', '$phone', '$address', '$description') ";
-            if ($connection->query($request)) {
+            // $request = "insert into book_form(date, topwear, bottomwear, woolen, others, servicetype, name, email, phone, address, description) values('$date','$topwear','$bottomwear','$woolen', '$others', '$servicetype', '$name', '$email', '$phone', '$address', '$description') ";
+            //corrected 
+            $stmt = $connection->prepare("insert into book_form (date, topwear, bottomwear, woolen, others, servicetype, name, email, phone, address, description) values (?,?,?,?,?,?,?,?,?,?,?)");
+            $stmt->bind_param("sssssssssss", $date, $topwear, $bottomwear, $woolen, $others, $servicetype, $name, $email, $phone, $address, $description);
+            if ($stmt->execute()) {
                $_POST = array();
                echo '<script>alert("Hurrah! your request has been submitted succesfully. Stay back we will connect with you shortly.");location.href="http://localhost:998/laundryms/home.php";</script>';
+           } else {
+               echo "Error: " . $request . "<br>" . $connection->error;
+               echo '<script>alert("Sorry! your request of cleaning session is failed. Please try again. ");location.href="http://localhost:998/laundryms/book.php";</script>';
+           }
 
-            } else {
-            echo "Error: " . $request . "<br>" . $connection->error;
-            echo '<script>alert("Sorry! your request of cleaning session is failed. Please try again. ");location.href="http://localhost:998/laundryms/book.php";</script>';
+            // if ($connection->query($request)) {
+            //    $_POST = array();
+            //    echo '<script>alert("Hurrah! your request has been submitted succesfully. Stay back we will connect with you shortly.");location.href="http://localhost:998/laundryms/home.php";</script>';
 
-            }
+            // } else {
+            // $request = htmlspecialchars($request);
+            // echo "Error: " . $request . "<br>" . $connection->error;
+            // echo '<script>alert("Sorry! your request of cleaning session is failed. Please try again. ");location.href="http://localhost:998/laundryms/book.php";</script>';
+
+            // }
          }
       }else{
          echo 'something went wrong please try again!';
